34a710262bfe9dc481dfd204eef95600dff60803
tomoko kanamori <humpty.dumpty.wt@gmail.com>
2018-11-27 07:16:53 +0000

    　CreateFavoritesテーブル修正

--- db/migrate/20181119061856_create_favorites.rb
+++ db/migrate/20181119061856_create_favorites.rb
@@ -3,8 +3,6 @@ class CreateFavorites < ActiveRecord::Migration[5.1]
     create_table :favorites do |t|
       t.integer :user_id, index: true
       t.integer :topic_id, index: true
-      t.index  :user_id, name:index_favorites_on_user_id
-      t.index :topic_id, name:index_favorites_on_topic_id
 
       t.timestamps
     end
