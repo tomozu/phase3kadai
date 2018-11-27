f155a87fe8820499c43997571a4709965672b523
tomoko kanamori <humpty.dumpty.wt@gmail.com>
2018-11-27 09:57:12 +0000

    ActionMailer機能修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  config.action_mailer.default_url_options = { host:'phase3-daikadai-tomokok.c9users.io/' }
+  config.action_mailer.default_url_options = { host: 'morning-atoll-43852.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :letter_opener_web
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
   config.action_mailer.default_url_options = { host: 'morning-atoll-43852.herokuapp.com/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
