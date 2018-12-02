b1f3d746ab7f46c086463d28e8f3828fd2af61b1
tomoko kanamori <humpty.dumpty.wt@gmail.com>
2018-12-02 02:09:44 +0000

    “バリデーション修正”

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,5 @@ class Topic < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   mount_uploader :image, ImageUploader
+  validates :image, presence: true
 end
